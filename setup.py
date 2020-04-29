dabbd999c72fbbd8dc8e425671f8527ae659b141
Andrzej Skrodki <andrzej.skrodzki@gmail.com>
2020-04-29 11:18:58 +0200

    First commit.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='sample',
+      version='0.1.0',
+      description='A sample project',
+      install_requires=['numpy>=1.18.2'],
+      zip_safe=False,
+      python_requires='>=3.6'
+)
