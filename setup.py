0850ea0b253456cdf5741e3bdbcbff1c3585769b
Andrzej Skrodki <andrzej.skrodzki@gmail.com>
2020-04-29 13:04:08 +0200

    update.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
       name='sample',
-      version='0.2.1',
+      version='0.3.0',
       description='A sample project',
       install_requires=['numpy>=1.18.2'],
       zip_safe=False,
